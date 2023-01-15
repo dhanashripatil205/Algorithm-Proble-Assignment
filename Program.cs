84299219a36b4d23564c062a55e1c32884926d3a
dhanashripatil205 <dhanashripatil205@gmail.com>
2023-01-15 13:32:11 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SortingTechniques
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("***** Welcome to Algorithm Problem Assignment. *****");
+        }
+    }
+}
+
+
+
