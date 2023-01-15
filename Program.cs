5fe5635cda2ee3e93d7dbb8bd889887bd24d2eee
dhanashripatil205 <dhanashripatil205@gmail.com>
2023-01-15 17:26:27 +0530

    Method calling

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         public static void Main(string[] args)
         {
-            PrimeNoInGIvenRange.FindPrimeNo();
+
+            //String str = "DOG";
+            //int n = str.Length;
+            //Console.Write("\nAll possible strings are : ");
+            //PermutationOfStringUsingIterativeMethod.usingIterativeMethod(str, 0, n - 1);
+            String s = "ABC";
+            String answer = "";
+
+            Console.Write("\nAll possible strings are : ");
+            PermutationOfStringUsingRecursiveApproach.RecursiveApproach(s, answer);
+
+
 
         }
 
