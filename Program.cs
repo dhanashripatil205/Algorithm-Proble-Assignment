8c41c96940cff194e1160cbd93d6cd5ab330cce5
dhanashripatil205 <dhanashripatil205@gmail.com>
2023-01-15 15:26:18 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -4,14 +4,12 @@
     {
         public static void Main(string[] args)
         {
-            SearchTheWordFromWordList d = new SearchTheWordFromWordList();
-            d.List();
-            Console.WriteLine("\nEnter word do you want to search : ");
-            string Word = Console.ReadLine();
-            d.Searching(Word);
+            InsertionSort.DisplayInsertionSort();
 
         }
+
     }
+
 }
 
 
