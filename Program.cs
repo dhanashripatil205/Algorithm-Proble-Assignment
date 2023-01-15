925b0d1eb2b5e5a03ee1c0f79cbe875aacb0338b
dhanashripatil205 <dhanashripatil205@gmail.com>
2023-01-15 15:41:43 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -4,13 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            int[] arr = { 12, 11, 13, 5, 6, 7 };
-            Console.WriteLine("Given Array");
-            MergeSort.printArray(arr);
-            MergeSort obja = new MergeSort();
-            obja.sort(arr, 0, arr.Length - 1);
-            Console.WriteLine("\nSorted array");
-            MergeSort.printArray(arr);
+            Anagram.DisplayAnagram();
 
         }
 
